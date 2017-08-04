16eeb91e1058fe58461c4c9758e62264082a9e0d
FateXRebirth <FateXRebirth@gmail.com>
2017-08-04 08:38:07 +0800

    New header design

--- imports/components/Header.jsx
+++ imports/components/Header.jsx
@@ -27,7 +27,17 @@ export default class Header extends React.Component {
         const user = this.state.user;
         return (
             <header className="header">
-
+                { loggingIn ? 
+                (<div className="ui container">
+                    <p className="title">Welcome!</p>
+                    <p className="content">My name is Daniel Imms, I'm a software engineer from Australia. I live in Redmond and work at Microsoft on Visual Studio Code. I use this blog as a platform to learn, revise and teach various software development topics.</p>
+                </div>) : 
+                (<div className="ui container">
+                    <p className="title">Create a unique blog</p>
+                    <p className="content">Start publishing in seconds. Instantly create the personal or professional blog of your dreams to share your ideas on the web.</p>
+                    <button className="ui positive button">Get Started</button>
+                </div>) 
+                }                
             </header> 
         )
     }
